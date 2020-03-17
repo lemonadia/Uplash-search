653f60e0d59b3678a313f4140fdb7d7a52904fae
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2020-03-17 17:39:42 +0100

    Add function addResult

--- search.js
+++ search.js
@@ -1,10 +1,12 @@
 const formTag = document.querySelector("form");
 const inputTag = formTag.querySelector("input"); //just in case we have more input in the page
+const resultsTag = document.querySelector("section.results");
+
 const accessKey = "tHSICgFNUZYtXRwNcuJY6czODrEbLFE-EXfuYDhfbmc";
-const apiUrl = "https://api.unsplash.com/search/photos?per_page=24&query=blue";
+const apiUrl = "https://api.unsplash.com/search/photos?per_page=24&query=";
 
 const searchUnsplash = function(term) {
-  return fetch(apiUrl, {
+  return fetch(apiUrl + term, {
     method: "GET",
     headers: {
       Authorization: "Client-ID " + accessKey
@@ -15,10 +17,37 @@ const searchUnsplash = function(term) {
       //formating the results to suit the needs
       return data.results.map(result => {
         return {
-          imageSrc: result.urls.regular
+          imageSrc: result.urls.regular,
+          width: result.width,
+          height: result.height,
+          name: result.user.name,
+          title: result.description || "Untitled",
+          color: (result.color || "#cccccc") + "33"
         };
       });
     });
+};
+
+// add result to the page
+
+const addResults = function(results) {
+  //remve all the loadin tags
+  resultsTag.innerHTML = "";
+  //loop over each individual result and add the result tag
+  results.forEach(result => {
+    resultsTag.innerHTML =
+      resultsTag.innerHTML +
+      `
+      <div class="single-result">
+        <div class="image" style="background-color: ${result.color}">
+          <img src="${result.imageSrc}">
+        </div>
+        <h2>${result.title}</h2>
+        <p>by ${result.name} - ${result.width} x ${result.height}</p>
+      </div>
+
+    `;
+  });
 };
 
 // when we submit the form get the info from input
@@ -26,6 +55,8 @@ formTag.addEventListener("submit", function(event) {
   //get the info from input
   const searchTerm = inputTag.value;
 
-  searchUnsplash(searchTerm);
+  searchUnsplash(searchTerm).then(results => {
+    addResults(results);
+  });
   event.preventDefault();
 });
